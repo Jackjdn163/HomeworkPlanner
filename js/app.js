03c47ed552b1ad3a191b3923a103ebfe79d9213b
Jackjdn163 <159971083+Jackjdn163@users.noreply.github.com>
2026-05-06 12:18:46 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,31 @@
 let schedule = JSON.parse(
   localStorage.getItem("schedule")
 ) || {
-  A:["Math","English","History","Science"],
-  B:["Theatre","Spanish","Elective","Health/Fitness"]
+
+  A:[
+    "Math",
+    "English",
+    "History",
+    "Science"
+  ],
+
+  B:[
+    "Theatre",
+    "Spanish",
+    "Elective",
+    "Health/Fitness"
+  ]
+
 };
+
+/* DATE */
 
 document
 .getElementById("todayText")
 .innerHTML =
-new Date().toLocaleDateString(
+
+new Date()
+.toLocaleDateString(
   "en-US",
   {
     weekday:"long",
@@ -17,14 +34,99 @@ new Date().toLocaleDateString(
   }
 );
 
+/* CLASS DROPDOWN */
+
 classes.forEach(c=>{
 
   document
   .getElementById("class")
   .innerHTML += `
-    <option>${c}</option>
+
+    <option>
+      ${c}
+    </option>
+
   `;
+
 });
 
+/* SETUP DROPDOWNS */
+
+[
+"a1","a2","a3","a4",
+"b1","b2","b3","b4"
+]
+
+.forEach(id=>{
+
+  const el =
+  document.getElementById(id);
+
+  classes.forEach(c=>{
+
+    el.innerHTML += `
+
+      <option>
+        ${c}
+      </option>
+
+    `;
+
+  });
+
+});
+
+/* SETUP SCREEN */
+
+if(
+  !localStorage.getItem("schedule")
+){
+
+  document
+  .getElementById("setup")
+  .classList.remove("hidden");
+
+}
+
+/* SAVE SCHEDULE */
+
+function saveSchedule(){
+
+  schedule = {
+
+    A:[
+      a1.value,
+      a2.value,
+      a3.value,
+      a4.value
+    ],
+
+    B:[
+      b1.value,
+      b2.value,
+      b3.value,
+      b4.value
+    ]
+
+  };
+
+  localStorage.setItem(
+    "schedule",
+    JSON.stringify(schedule)
+  );
+
+  document
+  .getElementById("setup")
+  .classList.add("hidden");
+
+  renderWeek();
+
+}
+
+/* STARTUP */
+
 renderWeek();
+
 updateDateFields();
+
+initializeAI();
