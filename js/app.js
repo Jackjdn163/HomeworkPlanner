17d3c08d9d945c7975fe6498db2170c49f04080d
Jackjdn163 <159971083+Jackjdn163@users.noreply.github.com>
2026-05-06 12:15:19 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
+let schedule = JSON.parse(
+  localStorage.getItem("schedule")
+) || {
+  A:["Math","English","History","Science"],
+  B:["Theatre","Spanish","Elective","Health/Fitness"]
+};
 
+document
+.getElementById("todayText")
+.innerHTML =
+new Date().toLocaleDateString(
+  "en-US",
+  {
+    weekday:"long",
+    month:"long",
+    day:"numeric"
+  }
+);
+
+classes.forEach(c=>{
+
+  document
+  .getElementById("class")
+  .innerHTML += `
+    <option>${c}</option>
+  `;
+});
+
+renderWeek();
+updateDateFields();
