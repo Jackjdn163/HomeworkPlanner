bf072fea1b9c7cd4696a18e28afc1956bc49774b
Jackjdn163 <159971083+Jackjdn163@users.noreply.github.com>
2026-05-06 12:12:02 -0400

    Update busy.js

--- js/busy.js
+++ js/busy.js
@@ -1,1 +1,25 @@
+let busy = JSON.parse(
+  localStorage.getItem("busy")
+) || [];
 
+function addBusy(){
+
+  busy.push({
+
+    id:Date.now(),
+
+    title:busyTitle.value,
+
+    repeat:repeat.value,
+
+    date:busyDate.value,
+
+    start:busyStart.value,
+
+    end:busyEnd.value
+
+  });
+
+  saveBusy();
+  renderWeek();
+}
