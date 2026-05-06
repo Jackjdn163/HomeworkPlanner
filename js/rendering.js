31e78550754c5e4622909d3b507ff36a637753a2
Jackjdn163 <159971083+Jackjdn163@users.noreply.github.com>
2026-05-06 12:13:03 -0400

    Update rendering.js

--- js/rendering.js
+++ js/rendering.js
@@ -1,1 +1,36 @@
+function hourToPixels(hour){
+  return (hour-8)*80;
+}
 
+function renderMajorAssignment(){
+
+  const banner =
+  document.getElementById(
+    "majorAssignmentBanner"
+  );
+
+  const major =
+  assignments
+  .filter(a=>
+    a.type === "Project" ||
+    a.type === "Essay" ||
+    a.type === "Test"
+  )
+  .sort((a,b)=>
+    new Date(a.due)-new Date(b.due)
+  )[0];
+
+  if(!major){
+
+    banner.innerHTML =
+    "No Major Assignments";
+
+    return;
+  }
+
+  banner.innerHTML = `
+    <h2>${major.title}</h2>
+    <p>${major.className}</p>
+    <p>Due ${major.due}</p>
+  `;
+}
