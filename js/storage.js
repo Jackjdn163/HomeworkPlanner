43ce5988d9c3e1875207f84a91ee29ded7f01131
Jackjdn163 <159971083+Jackjdn163@users.noreply.github.com>
2026-05-06 12:08:08 -0400

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,20 @@
+function saveAssignments(){
+  localStorage.setItem(
+    "assignments",
+    JSON.stringify(assignments)
+  );
+}
 
+function saveBusy(){
+  localStorage.setItem(
+    "busy",
+    JSON.stringify(busy)
+  );
+}
+
+function saveScheduleData(){
+  localStorage.setItem(
+    "schedule",
+    JSON.stringify(schedule)
+  );
+}
