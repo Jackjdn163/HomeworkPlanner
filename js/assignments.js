ea83953e490a518a2a6985d56b7d77037c78cbb2
Jackjdn163 <159971083+Jackjdn163@users.noreply.github.com>
2026-05-06 12:10:56 -0400

    Update assignments.js

--- js/assignments.js
+++ js/assignments.js
@@ -1,1 +1,44 @@
+let assignments = JSON.parse(
+  localStorage.getItem("assignments")
+) || [];
 
+function updateDateFields(){
+
+  const type =
+  document.getElementById("type").value;
+
+  document.getElementById(
+    "assignedWrapper"
+  ).style.display =
+  type === "Homework"
+  ? "block"
+  : "none";
+}
+
+function addAssignment(){
+
+  assignments.push({
+
+    id:Date.now(),
+
+    title:title.value,
+
+    className:
+    document.getElementById("class").value,
+
+    type:type.value,
+
+    assigned:
+    type.value === "Homework"
+    ? assigned.value
+    : null,
+
+    due:due.value,
+
+    hours:hours.value || 1
+
+  });
+
+  saveAssignments();
+  renderWeek();
+}
